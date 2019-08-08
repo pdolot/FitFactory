7791b63e2b4b4c10801b7279ca90a72654d2be04
pdolot <38375390+pdolot@users.noreply.github.com>
2019-08-08 12:31:52 +0200

    Create view for remember password fragment

--- app/src/main/java/com/example/fitfactory/presentation/fragments/rememberPasswordFragment/RememberPasswordFragment.kt
+++ app/src/main/java/com/example/fitfactory/presentation/fragments/rememberPasswordFragment/RememberPasswordFragment.kt
@@ -1,35 +1,37 @@
-package com.example.fitfactory.presentation.fragments.signUpFragment
+package com.example.fitfactory.presentation.fragments.rememberPasswordFragment
 
 import android.graphics.drawable.AnimatedVectorDrawable
 import android.graphics.drawable.Drawable
+import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.lifecycle.ViewModelProviders
+
 import com.example.fitfactory.R
-import com.example.fitfactory.presentation.base.BaseFragment
+import kotlinx.android.synthetic.main.remember_password_fragment.*
 import kotlinx.android.synthetic.main.sign_up_fragment.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class SignUpFragment : BaseFragment() {
+class RememberPasswordFragment : Fragment() {
 
-    private lateinit var viewModel: SignUpViewModel
+    private lateinit var viewModel: RememberPasswordViewModel
     private var animationList = ArrayList<AnimatedVectorDrawable>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SignUpViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(RememberPasswordViewModel::class.java)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.sign_up_fragment, container, false)
+        return inflater.inflate(R.layout.remember_password_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -38,15 +40,9 @@ class SignUpFragment : BaseFragment() {
     }
 
     private fun setListeners() {
-        signUpFragment_signUp.setOnClickListener { v ->
+        rememberPasswordFragment_rememberPassword.setOnClickListener { v ->
             animateView((v as ImageView).drawable)
-            signUpFragment_label.visibility = View.INVISIBLE
-            GlobalScope.launch {
-                delay(4000)
-                resetAnimations()
-                signUpFragment_signUp.setImageDrawable(resources.getDrawable(R.drawable.load_button_endanim_positive))
-                animateView(v.drawable)
-            }
+            rememberPasswordFragment_label.visibility = View.INVISIBLE
         }
     }
 
---
+++ app/src/main/java/com/example/fitfactory/presentation/fragments/rememberPasswordFragment/RememberPasswordViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.fitfactory.presentation.fragments.rememberPasswordFragment
+
+import androidx.lifecycle.ViewModel;
+
+class RememberPasswordViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
